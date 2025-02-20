b383618170965770f93df504533ddb7ff6fa15db
BobTarino <bob.tarino@gmail.com>
2025-02-20 13:13:30 -0500

    gave parameters to kaboom constant and added js files for utils and constants

--- src/kaboomCtx.js
+++ src/kaboomCtx.js
@@ -1,1 +1,7 @@
 import kaboom from "kaboom";
+
+export const k = kaboom({
+    global: false,
+    touchToMouse: true, /* transaltes all touch events on mobile to click events*/
+    canvas: document.getElementById("game") /* telling canvas to target the game element in html */
+})
