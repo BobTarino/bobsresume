804414771e28ffabb0f061c6da5ae485218ef31a
BobTarino <bob.tarino@gmail.com>
2025-02-20 18:49:05 -0500

    added scaleFactor constant in constants.js

--- src/main.js
+++ src/main.js
@@ -18,10 +18,10 @@ k.loadSprite("map", "./map.png");
 k.setBackground(k.Color.fromHex("311047"));
 
 k.scene("main", async () => {
-    const mapData = await (await fetch("./map")).json() /* we await fetch function because its async; code would continue to execute - we want to load map data and not move rest of code until it's done - then convert to json object - once data is loaded we are ready to move on */
+    const mapData = await (await fetch("./map")).json()   /* we await fetch function because its async; code would continue to execute - we want to load map data and not move rest of code until it's done - then convert to json object - once data is loaded we are ready to move on */
     const layers = mapData.layers;
 
-    const map = k.make([])
+    const map = k.make([k.sprite("map"), k.pos(0), k.scale()]);    /* map game object - make (makes game object) add (displays game object) */
 });
 
 k.go("main");
