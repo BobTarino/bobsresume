67279a22dffe0c2c7c363463bee8fc92e3c345ec
BobTarino <bob.tarino@gmail.com>
2025-02-20 13:55:15 -0500

    load sprite sheet function with paarmeters spritesheet dimensions

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import { k } from "./kaboomCtx";
+
+k.loadSprite("spritesheet", "./spritesheet.png", {
+    sliceX: 39, /* 39 frames on X axis (each frame is 16x16 tile) */
+    sliceY: 31, /* 31 frames on Y axis */
+    anims: {
+
+    }
+});
