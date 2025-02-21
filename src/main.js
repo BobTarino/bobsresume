509b94cfd6fba02cff722125eebeccfec3c666d3
BobTarino <bob.tarino@gmail.com>
2025-02-21 12:07:54 -0500

    wrote logic to iterate through layers and objects of collision

--- src/main.js
+++ src/main.js
@@ -27,7 +27,7 @@ k.scene("main", async () => {
     const player = k.make([
         k.sprite("spritesheet", { anim: "idle-down" }), /* import spritesheet and default animation is idle-down */
         k.area({
-            shape: new k.Rect(k.vec2(0,3, 10, 10)) /* hitbox will be a rectangle and first param is vector coordinates (hitbox drawn from origin of player plus 3 on x axis, and last two params are height and width of hitbox   )  */
+            shape: new k.Rect(k.vec2(0,3,), 10, 10) /* hitbox will be a rectangle and first param is vector coordinates (hitbox drawn from origin of player plus 3 on x axis, and last two params are height and width of hitbox   )  */
         }),
         k.body(), /* k.body is a component that makes player tangible physics object that can be collided with  */
         k.anchor("center"), /* draw player from center not top left corner (default) */
@@ -39,6 +39,21 @@ k.scene("main", async () => {
         },
         "player", /* identify game object in code - especially when making a collision event */
     ]);
+
+    for (const layer of layers) {   /* for loop iterates through various map.json layers */
+        if (layer.name === "boundaries") {  /* if layer name is boundaries */
+            for (const boundary of layer.objects) /* for loop iterates through all objects in "boundaries" layer  */ {
+                map.add([     /* map object and add game object */
+                    k.area({
+                       shape: new k.Rect(k.vec2(0), boundary.width, boundary.height), /* area component - coordinates of hitbox relative to game object (will be same position as game object,) width and height of boundary from Tiled export (map.json) */
+                    }),
+                    k.body({ isStatic: true }), /* body component - isStatic property makes sure that player will not be able to overlap with walls */
+                    k.pos(boundary.x, boundary.y), /* position of game object itself */
+                    boundary.name, /* acts as tag for game object - will come in handy when player collides with object such as "bed" or "PC" */
+                ]);
+            }
+        }
+    }
 });
 
 k.go("main");
