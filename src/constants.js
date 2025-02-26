cae1a02aa54cfad687d182b2db5881c1c419a158
BobTarino <bob.tarino@gmail.com>
2025-02-26 13:02:29 -0500

    dialouge data update

--- src/constants.js
+++ src/constants.js
@@ -2,13 +2,13 @@ export const scaleFactor = 4;
 export const dialogueData = {
     pc: `This is my PC. I work mostly in JavaScript these days.
       I've made a couple of apps in that language. I also like C++. Check out my <a href="https://github.com/BobTarino" target="_blank">Github</a>!`,
-    "cs-degree": `This is my Coding Certificate. <a href="https://github.com/BobTarino/port25/blob/main/assets/full-stack-coding-utah.png" target="_blank"I hung it on the wall because I'm proud of it.</a>`,
-    "sofa-table": `That's my sofa. I like to relax here and play video games like this <a href="https://bobtarino.github.io/tetris/" target="_blank"throwback</a>!`,
+    "cs-degree": `This is my Coding Certificate. <a href="https://github.com/BobTarino/port25/blob/main/assets/full-stack-coding-utah.png" target="_blank">I hung it on the wall because I'm proud of it.</a>`,
+    "sofa-table": `That's my sofa. I like to relax here and play video games like this <a href="https://bobtarino.github.io/tetris/" target="_blank">throwback</a>!`,
     tv: `That's my TV. I've been watching videos on YouTube and Twitch. 
         Hey! You can also watch me on Twitch! <a href="https://www.twitch.tv/chickentendo64" target="_blank">Follow me?</a>`,
     bed: `This where I sleep. I try to get 7 hours to 9 hours of sleep every night. I hope my sleep paralysis demon doesn't visit tonight...`,
     resume: `This is my desk and on it is my resume. <a href="https://github.com/BobTarino/port25/blob/main/assets/RTarino_Resume2025.pdf" target="_blank">Check it out?</a>
-    Contact me at bob.tarino@gmail.com if you have any questions (or opportunities)`,
+    Contact me at bob.tarino@gmail.com if you have any questions (or opportunities)!`,
     projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
     Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. By the way, I followed an awesome tutorial by @JSLegendDe to make this gamified portfolio. <a href="https://youtu.be/wy_fSStEgMs?si=dlOo95KreZUQQHF5" target="_blank">You can do it yourself!</a>`,
     library: `There are a lot of books about programming on this shelf. There are even articles I wrote for a website called Screen Rant from back in the day. You can read them <a href="https://screenrant.com/author/robert-tarino/" target="_blank">here!</a> `,
